db2c2544fc156e24925248835a65a01598fbba76
Viacheslav Tarabarin <slava1383@gmail.com>
2023-03-14 11:40:47 +0200

    Added hw modules from 1 to 3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,15 @@
 let calculator = {
   // ваш код
+  read(a, b) {
+    calculator.aCalc = a;
+    calculator.bCalc = b;
+  },
+  sum() {
+    return this.aCalc + this.bCalc;
+  },
+  mul() {
+    return this.aCalc * this.bCalc;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
